f9fe705018b9e64ec3142966871a2a8df6893fb5
Jisse Reitsma <jisse@yireo.com>
2025-09-25 15:38:04 +0200

    Adding all files to git via Yireo Command

---
+++ Console/Command/ModuleEnableSequenceCommand.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace Yireo\EnableModuleSequence\Console\Command;
+
+use Magento\Framework\Component\ComponentRegistrar;
+use SimpleXMLElement;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Yireo\ExtensionChecker\Exception\ModuleNotFoundException;
+use Symfony\Component\Console\Input\ArrayInput;
+
+class ModuleEnableSequenceCommand extends Command
+{
+    public function __construct(
+        private readonly ComponentRegistrar $componentRegistrar,
+        ?string $name = null)  {
+        parent::__construct($name);
+    }
+
+    protected function configure()
+    {
+        $this->setName('module:enable:sequence');
+        $this->setDescription('Enable a module and its module sequence at once');
+
+        $this->addArgument(
+            'module',
+            InputArgument::REQUIRED,
+            'Name of the module'
+        );
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $moduleName = $input->getArgument('module');
+        $moduleSequence = $this->getModuleSequence($moduleName);
+        $moduleSequence[] = $moduleName;
+
+        $cmd = $this->getApplication()->find('module:enable');
+
+        $input = new ArrayInput([
+            'module' => $moduleSequence
+        ]);
+
+        $input->setInteractive(false);
+        return $cmd->run($input, $output);
+    }
+
+    private function getModuleSequence(string $moduleName): array
+    {
+        $modulePath = $this->componentRegistrar->getPath(ComponentRegistrar::MODULE, $moduleName);
+        $moduleXmlFile = $modulePath.'/etc/module.xml';
+
+        $configNode = simplexml_load_file($moduleXmlFile);
+        $moduleSequence = [];
+        if ($configNode->module->sequence) {
+            foreach ($configNode->module->sequence->module as $sequenceModule) {
+                $moduleSequence[] = (string)$sequenceModule['name'];
+            }
+        }
+
+        return $moduleSequence;
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Yireo EnableModuleSequence
+
+**When running the Magento 2 command `bin/magento module:enable` for a given module, the dependencies of that module (as declared in the `sequence` of its `etc/module.xml` file) is not enabled. This is a good thing, because not always does the sequence reflect all neccessary dependencies. But if so, this module gives you a command to enable all dependencies at once.**
+
+### Installation
+```bash
+composer require yireo/magento2-enable-module-sequence
+bin/magento module:enable Yireo_EnableModuleSequence
+```
+
+### Usage
+```bash
+bin/magento module:enable:sequence Yireo_Example
+```
